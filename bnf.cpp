f5c412ebd8927a0b4f61ec50f7937e3e1c94b59b
faisazif <44775100+faisazif@users.noreply.github.com>
2018-11-05 20:12:24 +0700

    Add files via upload

---
+++ bnf.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <fstream>
+
+using namespace std;
+
+char symbol;
+bool done;
+ifstream inputFile;
+
+void Accept(char t)
+{
+	if(symbol == t)
+	{
+		printf("Accepted symbol: %c\n", symbol);
+		inputFile.get(symbol);
+	}
+	else
+	{
+		if(symbol == '\n')	
+			printf("Rejected. Symbol: enter\n");
+		else
+			printf("Rejected. Symbol: %c\n", symbol);
+		printf("Symbol should be: %c\n", t);
+		exit(0);
+	}
+}
+
+void EscapeCharacter()
+{
+	if(symbol == '\n')
+		Accept('\n');
+}
+
+void A()
+{
+	Accept('f');
+	Accept('a');
+	Accept('i');
+	Accept('s');
+	EscapeCharacter();
+}
+
+int main()
+{
+	done = false;
+	inputFile.open("input.txt");
+	if(!inputFile)
+	{
+		printf("File not found");
+		return 1;
+	}
+
+	inputFile.get(symbol);
+	A();
+
+	if(inputFile.peek()	== EOF)
+	{
+		printf("----------------------ZUCC----------------------\n\n\n");
+		done = true;
+	}
+	else
+	{
+		inputFile.get(symbol);
+		printf("Rejected. There are still symbol: %c\n", symbol);
+	}
+	return 0;
+}
+
