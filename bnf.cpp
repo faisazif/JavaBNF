2a9aba9512336c45f6367d2b1aaf94f0d8e2f10e
FaisZ <fais9999@ymail.com>
2018-11-09 10:25:36 +0700

    Rule 139-142 Added

--- bnf.cpp
+++ bnf.cpp
@@ -1,14 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> 
 #include <fstream>
-
+#include <ctype.h>
 using namespace std;
-
-char symbol;
-int errorLine;
-int errorCol;
-int defaultCol;
+char symbol; 
+int errorLine; 
+int errorCol; 
+int defaultCol; 
 ifstream inputFile;
+//Prototypes
+void VariableSymbol();void Letter();void Digit();
+
+int main(){
+	defaultCol = 1;
+	errorLine = defaultCol;
+	errorCol = defaultCol;
+	inputFile.open("input.txt");
+	if(!inputFile) {
+		printf("Error: File not found");
+		return 1;
+	}
+
+	inputFile.get(symbol);
+	VariableSymbol();
+	//A();
+
+	//Check if there are more symbols 
+	{
+		if(inputFile.eof())
+			printf("\n----------------------ZUCC----------------------\n");
+		else {
+			printf("Rejected on line %d, column %d\n", errorLine, errorCol);
+			printf("Reason:\nThere are still symbol: %c\n", symbol);
+		}
+	}
+	return 0;
+}
 
 void Accept(char t){
 	if(inputFile.eof())
@@ -35,6 +62,59 @@ void Accept(char t){
 	}
 }
 
+
+//Rules
+void NextLetter(){
+	if (isalpha(symbol)){
+		Letter(); NextLetter();
+	} 
+	else if(isdigit(symbol)){
+		Digit(); NextLetter();
+	}
+	else if(symbol == '$' || symbol == '_'){
+		VariableSymbol(); NextLetter();
+	}
+}
+void VariableSymbol() {
+	switch(symbol)
+	{
+		case '$' : Accept('$'); break;
+		default : Accept('_');
+	}
+}
+void Letter() {
+	if(isalpha(symbol))
+		Accept(symbol);
+	else
+	{
+		printf("Rejected on line %d, column %d\n", errorLine, errorCol);
+		printf("Reason:");
+		if(symbol == '\n')	
+			printf("Symbol found: enter\n");
+		else
+			printf("Symbol found: %c\n", symbol);
+		printf("Symbol should be a letter\n");
+		exit(0);
+	}
+}
+void Digit() {
+	if(isdigit(symbol))
+		Accept(symbol);
+	else
+	{
+		printf("Rejected on line %d, column %d\n", errorLine, errorCol);
+		printf("Reason:");
+		if(symbol == '\n')	
+			printf("Symbol found: enter\n");
+		else
+			printf("Symbol found: %c\n", symbol);
+		printf("Symbol should be a digit\n");
+		exit(0);
+	}
+}
+
+
+//TestRules
 void EscapeCharacter(){
 	while (symbol == '\n')
 	{
@@ -45,7 +125,6 @@ void EscapeCharacter(){
 			break;
 	}
 }
-
 void A(){
 	while(symbol == 'f')
 	{
@@ -55,28 +134,5 @@ void A(){
 		Accept('s');
 		EscapeCharacter();
 	}
-}
-
-int main()
-{
-	defaultCol = 1;
-	errorLine = defaultCol;
-	errorCol = defaultCol;
-	inputFile.open("input.txt");
-	if(!inputFile) {
-		printf("Error: File not found");
-		return 1;
-	}
-
-	inputFile.get(symbol);
-	A();
-
-	if(inputFile.eof())
-		printf("\n----------------------ZUCC----------------------\n");
-	else {
-		printf("Rejected on line %d, column %d\n", errorLine, errorCol);
-		printf("Reason:\nThere are still symbol: %c\n", symbol);
-	}
-	return 0;
 }
 
